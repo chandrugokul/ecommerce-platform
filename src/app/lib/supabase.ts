6551f40728f8e8224332fe3ffbada24f9fbd04bf
chandru <46309636+chandrugokul@users.noreply.github.com>
2026-09-01 23:36:10 +0530

    Delete supabase.ts

--- src/app/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  process.env.https://mhrqwfsisngpmnjgzxjx.supabase.co,
-    process.env.sb_publishable_GJ5PTpZ5AcUj4yaG3O45lA_zqHzBbF0
-    )
