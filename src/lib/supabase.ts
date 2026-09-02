8f12514c78ee8061423ea561215db46838c7dc89
chandru <46309636+chandrugokul@users.noreply.github.com>
2026-09-02 10:56:24 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.https://mhrqwfsisngpmnjgzxjx.supabase.co,
-    process.env.sb_publishable_GJ5PTpZ5AcUj4yaG3O45lA_zqHzBbF0
-    )
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
